d93d1771321ce3dd9798d0c929eb828d34862839
Jrowarth10 <117270540+Jrowarth10@users.noreply.github.com>
2024-03-01 16:01:51 +0100

    Got the post to work

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/", async (req, res) => {
 
     // Wait for all requests to complete
     const artDetails = await Promise.all(artWork);
-    console.log(artDetails)
+    // console.log(artDetails);
 
     res.render("index.ejs", { content: artDetails });
   } catch (error) {
@@ -33,9 +33,16 @@ app.get("/", async (req, res) => {
     res.status(500).send("Error fetching data.");
   }
 });
-app.get("/department", async (req, res) => {
+
+
+app.post("/submit", async (req, res) => {
   try {
-    const result = await axios.get(`${API_DEPARTMENTS_URL}4`);
+    // Retrieve the department ID from the submitted form data
+    const departmentId = req.body.departmentChoice;
+    console.log(departmentId)
+
+    // Fetch data for the selected department
+    const result = await axios.get(`${API_DEPARTMENTS_URL}${departmentId}`);
     const objectIds = result.data.objectIDs.slice(0, 10); // Limiting to first 10 object IDs for demonstration
 
     // Fetch details for each object in parallel
@@ -46,36 +53,15 @@ app.get("/department", async (req, res) => {
 
     // Wait for all requests to complete
     const artDetails = await Promise.all(artWork);
-    console.log(artDetails)
+    console.log(artDetails);
 
+    // Render the department page with fetched data
     res.render("department.ejs", { content: artDetails });
   } catch (error) {
     console.log(error);
     res.status(500).send("Error fetching data.");
   }
 });
-
-
-// app.get("/", async (req, res) => {
-//   try {
-//     const result = await axios.get(API_URL);
-//     const objectIds = result.data.objectIDs.slice(1899, 2000); // Limiting to first 10 object IDs for demonstration
-
-//     // Fetch details for each object in parallel
-//     const artWork = objectIds.map(async (objectId) => {
-//       const response = await axios.get(`${API_URL}${objectId}`);
-//       return response.data;
-//     });
-
-//     // Wait for all requests to complete
-//     const artDetails = await Promise.all(artWork);
-
-//     res.render("index.ejs", { content: artDetails });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).send("Error fetching data.");
-//   }
-// });
 
 //https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=5
 
